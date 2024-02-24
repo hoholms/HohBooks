cfd093dcae6b10fb5531dbddc83336869ab52973
hoholms <ninikabug@gmail.com>
2024-02-24 02:52:21 +0200

    Add cards

---
+++ app/src/main/java/com/hoholms/book/hohbooks/BookCard.kt
@@ -0,0 +1,91 @@
+package com.hoholms.book.hohbooks
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+data class Book(
+    val cover: Int = R.drawable.ic_launcher_foreground,
+    val title: String = "Book",
+    val author: String = "Author",
+    val description: String = "Description"
+)
+
+@Composable
+fun BookCard(book: Book = Book(), onClick1: () -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        ),
+        onClick = onClick1
+    ) {
+        Row(
+//            modifier = Modifier
+//                .background(MaterialTheme.colorScheme.secondaryContainer),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Left side: Book Cover
+            Image(
+                painter = painterResource(id = book.cover),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop,
+            )
+
+            // Right side: Title, Author, and Description
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = book.title,
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                Text(
+                    text = "by ${book.author}",
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                Text(
+                    text = book.description, style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewBookCard() {
+    val book = Book(
+        cover = R.drawable.ic_launcher_foreground,
+        title = "Sample Book Title",
+        author = "John Doe",
+        description = "A sample book description for preview purposes. This is a long description that provides more information about the book."
+    )
+
+    BookCard(book = book)
+}
--- app/src/main/java/com/hoholms/book/hohbooks/screens/BooksScreen.kt
+++ app/src/main/java/com/hoholms/book/hohbooks/screens/BooksScreen.kt
@@ -4,10 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Celebration
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,6 +26,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.hoholms.book.hohbooks.Book
+import com.hoholms.book.hohbooks.BookCard
 import com.hoholms.book.hohbooks.R
 import com.hoholms.book.hohbooks.ui.theme.HohBooksTheme
 import com.hoholms.book.hohbooks.viewmodel.ThemeViewModel
@@ -40,6 +43,7 @@ fun BooksScreen(navController: NavController, themeViewModel: ThemeViewModel = v
             Column(
                 modifier = Modifier
                     .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
                     .padding(15.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
@@ -50,10 +54,6 @@ fun BooksScreen(navController: NavController, themeViewModel: ThemeViewModel = v
                     modifier = Modifier.padding(vertical = 20.dp)
                 )
 
-                Button(onClick = { showDialog = true }) {
-                    Text("Press me")
-                }
-
                 if (showDialog) {
                     AlertDialogExample(
                         onDismissRequest = { showDialog = false },
@@ -63,6 +63,54 @@ fun BooksScreen(navController: NavController, themeViewModel: ThemeViewModel = v
                         icon = Icons.Filled.Celebration
                     )
                 }
+
+                val books = listOf(
+                    Book(
+                        title = "The Alchemist",
+                        author = "Paulo Coelho",
+                        description = "A mystical story about an Andalusian shepherd boy named Santiago who travels from his homeland in Spain to the Egyptian pyramids in search of a worldly treasure."
+                    ), Book(
+                        title = "To Kill a Mockingbird",
+                        author = "Harper Lee",
+                        description = "An American novel and classic work of Southern Gothic literature that was first published in 1960. It was Harper Lee's first and only published novel."
+                    ), Book(
+                        title = "Pride and Prejudice",
+                        author = "Jane Austen",
+                        description = "A romantic novel of manners by Jane Austen, first published in 1813 under the title 'First Impressions'. The story follows the main character Elizabeth Bennet as she deals with issues of manners, upbringing, morality, education and marriage."
+                    ), Book(
+                        title = "One Hundred Years of Solitude",
+                        author = "Gabriel Garcia Marquez",
+                        description = "The novel tells the multi-generational story of the Buendía family's decadence under the tropical sun of Macondo,"
+                    ), Book(
+                        title = "Jane Eyre",
+                        author = "Charlotte Bronte",
+                        description = "An English novel by Charlotte Brontë, published on 16 October 1847 under the pseudonym 'Currer Bell'. The first person narrative follows the experiences of its heroine, Jane Eyre, from her childhood through to adulthood."
+                    ), Book(
+                        title = "The Great Gatsby",
+                        author = "F. Scott Fitzgerald",
+                        description = "An American novel by F. Scott Fitzgerald published in 1925, set in the summer of 1922. The story is narrated by Nick Carraway."
+                    ), Book(
+                        title = "Brave New World",
+                        author = "Aldous Huxley",
+                        description = "A dystopian novel written in 1931 and published in 1932, set in a futuristic society where people are produced in an industrial environment."
+                    ), Book(
+                        title = "The Catcher in the Rye",
+                        author = "J.D. Salinger",
+                        description = "A first-person narrative told by Holden Caulfield, who is an antihero seeking moral clarity and understanding between adolescence and adulthood as he faces the harsh realities of the world."
+                    ), Book(
+                        title = "Beloved",
+                        author = "Toni Morrison",
+                        description = "A novel by American author Toni Morrison. It was published in 1987, for which she won the Pulitzer Prize and National Book Critics Circle Award."
+                    ), Book(
+                        title = "The Hobbit",
+                        author = "J.R.R Tolkien",
+                        description = "An epic fantasy novel by J. R. R. Tolkien that was published in September 1937 to unprecedented critical acclaim."
+                    )
+                )
+                books.forEach { book: Book ->
+                    BookCard(book = book, onClick1 = { showDialog = true })
+                }
+
             }
         }
     }
@@ -76,36 +124,25 @@ fun AlertDialogExample(
     dialogText: String,
     icon: ImageVector,
 ) {
-    AlertDialog(
-        icon = {
-            Icon(icon, contentDescription = "Example Icon")
-        },
-        title = {
-            Text(text = dialogTitle)
-        },
-        text = {
-            Text(text = dialogText)
-        },
-        onDismissRequest = {
-            onDismissRequest()
-        },
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    onConfirmation()
-                }
-            ) {
-                Text("Confirm")
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = {
-                    onDismissRequest()
-                }
-            ) {
-                Text("Dismiss")
-            }
+    AlertDialog(icon = {
+        Icon(icon, contentDescription = "Example Icon")
+    }, title = {
+        Text(text = dialogTitle)
+    }, text = {
+        Text(text = dialogText)
+    }, onDismissRequest = {
+        onDismissRequest()
+    }, confirmButton = {
+        TextButton(onClick = {
+            onConfirmation()
+        }) {
+            Text("Confirm")
         }
-    )
+    }, dismissButton = {
+        TextButton(onClick = {
+            onDismissRequest()
+        }) {
+            Text("Dismiss")
+        }
+    })
 }
